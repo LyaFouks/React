c3a581105b80c720b064678f58f06401980e9c77
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-09-17 19:29:40 +0300

    Create "state"

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -3,24 +3,32 @@ import './App.css';
 import Car from './Car/Car'
 
 class App extends Component {
+
+    state = {
+        cars: [
+            {name: 'Ford', year: 2018},
+            {name: 'Audi', year: 2016},
+            {name: 'Mazda 3', year: 2010}
+        ],
+        pageTittle: 'React components'
+    }
+
     render() {
         const divStyle = {
             textAlign: 'center'
         }
 
+        const cars = this.state.cars
+
 
 
         return (
             <div style={divStyle}>
-                <h1>Hello world!</h1>
+                <h1>{this.state.pageTittle}</h1>
 
-                <Car name={'Ford'} year={2018}>
-                    <p style={{color: 'aliceblue'}}>COLOR</p>
-                </Car>
-                <Car name="Audi" year={2016}>
-                    <p style={{color: 'indianred'}}>COLOR</p>
-                </Car>
-                <Car name={'Mazda'} year={2010}/>
+                <Car name={cars[0].name} year={cars[0].year} />
+                <Car name={cars[1].name} year={cars[1].year} />
+                <Car name={cars[2].name} year={cars[2].year} />
             </div>
         );
     }
